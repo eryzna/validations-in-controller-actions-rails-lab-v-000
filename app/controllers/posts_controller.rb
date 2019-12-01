f2d238fc78b99a4c67c23fa67bbd005c7d366bcc
Elana Ryznar <eryzna@gmail.com>
2019-12-01 23:06:01 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post= Post.find_by(params[:id])
+    @post= set_post!
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
